a870025b85bdb6647a8dc479e8083509ffc0570f
Mike Ricos via ux305-3 <mike.ricos@gmail.com>
2020-11-22 17:28:14 -0800

    Adds cut-and-paste React Bash functions.

---
+++ protocard/cpr.sh
@@ -0,0 +1,36 @@
+cpr-make-page(){
+CPR_DATE=$(date)
+cat <<EOF
+<html> 
+<script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+<script crossorigin src="https://unpkg.com/babel-standalone@6.26.0/babel.js"></script>
+<!-- Following from: https://reactjs.org/docs/state-and-lifecycle.html -->
+<div id='root'></div>
+<script type="text/babel">
+function tick() {
+  let element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <p>Generated from Cut and Paste React at $CPR_DATE.
+    </div>
+  );  
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+setInterval(tick, 1000);
+</script>
+</html>
+EOF
+}
+
+cpr-serve(){
+echo ""
+echo "Copy the following and paste into your URL of browser:"
+echo ""
+echo "data:text/html,"
+cpr-make-page
+}
---
+++ protocard/readme.md
@@ -0,0 +1,12 @@
+# Flashcard app using serveless React.
+
+## Copy and paste React
+- [Copy and Paste React without a server](https://hackmd.io/xLdVCNqdQCK8_6U6PZa1EA): React without a Server, a.k.a. copy&paste React using `data:text/html`
+
+## 1. apptool.sh: Bash CLI tool for creating cut and paste apps. 
+- To get started: `source apptool.sh`
+- `cpr-prefix; cpr-make-page`
+- copy text to browser URL
+- checkout cpr-make-page in [cpr.sh](./cpr.sh)
+
+## 2.  
