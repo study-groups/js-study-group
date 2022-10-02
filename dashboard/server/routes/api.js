e23a16100d717695d49061c40a8981e36c8b7fe8
Mike Ricos <mike.ricos@gmail.com>
2022-10-02 11:53:01 -0700

    Adds api route

--- dashboard/server/routes/api.js
+++ dashboard/server/routes/api.js
@@ -1,10 +1,15 @@
 import express from "express";
 const router = express.Router();
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // http://IP:PORT/api
 router.get("/", function(req, res, next) {
-    res.sendFile("tsd.html", {root: path.join(__dirname, "./public")})    
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify( {data:"testing"} ));
 });
 
 router.get("/a", function (req, res) {
